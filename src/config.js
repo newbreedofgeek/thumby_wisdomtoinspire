48a8f32dcd5607ce17995cc1b912c779db1c0d88
Mark Paul <paul.mark@gmail.com>
2016-08-12 11:05:33 +1000

    added the skipImgTs option from thumby

--- src/config.js
+++ src/config.js
@@ -2,9 +2,10 @@ export default {
   env: 'production',
   port: 7000,
   trashOriginal: true,
-  storageRoot: '/storage',
+  storageRoot: '/storage', // or user "/public/imgs" if a mounted directory is used. ** see below
   lrgWidth: 600,
   medWidth: 312,
   smlWidth: 160,
-  imgCache: '31536000' // cache imgs for 8 hours here
+  imgCache: 31536000, // cache imgs for 8 hours here
+  skipImgTs: true // dont append a timestamp on the physical image name
 };
